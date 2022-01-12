182101454c28408ed229ab58c94690e53d75550d
Roberto <robertodefreitas@users.noreply.github.com>
2022-01-12 18:03:31 +0100

    file renamed

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/CritterTestController.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/CritterTestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
  * your project work.
  */
 @RestController
-public class CritterController {
+public class CritterTestController {
 
     @GetMapping("/test")
     public String test(){
