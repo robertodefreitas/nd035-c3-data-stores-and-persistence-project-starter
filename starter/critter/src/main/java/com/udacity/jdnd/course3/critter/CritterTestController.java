28e9ab31ed81b35b51a51d6c3c39b73b04c1dd41
Roberto <robertodefreitas@users.noreply.github.com>
2022-01-18 18:04:48 +0100

    add log into CritterTestController.java

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/CritterTestController.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/CritterTestController.java
@@ -1,5 +1,7 @@
 package com.udacity.jdnd.course3.critter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CritterTestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(CritterTestController.class);
+
     @GetMapping("/test")
     public String test(){
+        String methodeName = new Object(){}.getClass().getEnclosingMethod().getName();
+        logger.info("[{}] GET /test", methodeName);
+
         return "Critter Starter installed successfully";
     }
 }
